dce01db43417354d254e78e1e419086cc80d1fe8
Higor Braga <higorbraga00@gmail.com>
2020-11-25 23:15:00 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/nelioalves/mc/config/DevConfig.java
+++ src/main/java/com/nelioalves/mc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.nelioalves.mc.services.DBService;
+import com.nelioalves.mc.services.EmailService;
+import com.nelioalves.mc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -27,5 +29,10 @@ public class DevConfig {
 		}
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService()	{
+		return new SmtpEmailService();
 	}
 }
