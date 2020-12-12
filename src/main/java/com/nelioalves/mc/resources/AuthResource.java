3e92db2136f3fdb8a10d88175a591b892c4018c3
Higor Braga <higorbraga00@gmail.com>
2020-12-11 22:06:02 -0300

    Refresh token

---
+++ src/main/java/com/nelioalves/mc/resources/AuthResource.java
@@ -0,0 +1,26 @@
+package com.nelioalves.mc.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.nelioalves.mc.security.JWTUtil;
+import com.nelioalves.mc.security.UserSS;
+import com.nelioalves.mc.services.UserService;
+
+public class AuthResource {
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+}
