8d531baaf6bcd4d3db60ba210441885a066cf774
Higor Braga <higorbraga00@gmail.com>
2021-01-14 00:40:26 -0300

    Enviando imagem via endpoint

--- src/main/java/com/nelioalves/mc/services/ClienteService.java
+++ src/main/java/com/nelioalves/mc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.nelioalves.mc.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,6 +11,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.nelioalves.mc.domain.Cidade;
 import com.nelioalves.mc.domain.Cliente;
@@ -37,6 +39,9 @@ public class ClienteService {
 	@Autowired
 	private EnderecoRepository enderecoRepository;
 
+	@Autowired
+	private S3Service s3Service;
+	
 	public Cliente find(Integer id) {
 		UserSS user = UserService.authenticated();
 		if(user==null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
@@ -102,6 +107,10 @@ public class ClienteService {
 	private void updateDate(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());		
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 
 }
