e8bedfdb20c0f61c06269eb9337f458237820c4a
Higor Braga <higorbraga00@gmail.com>
2021-01-15 00:17:30 -0300

    Usando padrao de nomes para imagens

--- src/main/java/com/nelioalves/mc/domain/Cliente.java
+++ src/main/java/com/nelioalves/mc/domain/Cliente.java
@@ -54,8 +54,6 @@ public class Cliente implements Serializable{
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
-	private String imageUrl;
-	
 	public Cliente() {
 		addPerfil(Perfil.CLIENTE);
 	}
@@ -145,14 +143,6 @@ public class Cliente implements Serializable{
 	
 	public Set<Perfil> getPerfis(){
 		return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
-	}
-	
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
 	}
 
 	public void addPerfil(Perfil perfil) {
